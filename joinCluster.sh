57127a26ce4c78f00ede417d60786ac37b4ff7f0
zsx <thinkernel@gmail.com>
2015-06-23 10:24:48 +0000

    Switch listening IP to 0.0.0.0.

--- joinCluster.sh
+++ joinCluster.sh
@@ -4,13 +4,13 @@ set -e
 HOST_IP=$1
 JOIN_IP=$2
 docker run --name consul -h $HOSTNAME \
-    -p ${HOST_IP}:8300:8300 \
-    -p ${HOST_IP}:8301:8301 \
-    -p ${HOST_IP}:8301:8301/udp \
-    -p ${HOST_IP}:8302:8302 \
-    -p ${HOST_IP}:8302:8302/udp \
-    -p ${HOST_IP}:8400:8400 \
-    -p ${HOST_IP}:8500:8500 \
+    -p 8300:8300 \
+    -p 8301:8301 \
+    -p 8301:8301/udp \
+    -p 8302:8302 \
+    -p 8302:8302/udp \
+    -p 8400:8400 \
+    -p 8500:8500 \
     -p 172.17.42.1:53:53 \
     -p 172.17.42.1:53:53/udp \
     -d progrium/consul -server -advertise ${HOST_IP} \
